fb75f323a71e70ab2cb5772182612177f6a0ae96
Wenyu Zhou <zwyhou@163.com>
2026-05-25 09:59:33 +0800

    add 1-7practise
    添加Ray Tracing 的练习

---
+++ RayTracing_5_adding_sphere/camera.h
@@ -0,0 +1,15 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "hittable.h"
+
+class camera{
+    public:
+        //public camera parameter here
+
+        void render(const hittable& world){
+            
+        }
+};
+
+#endif
